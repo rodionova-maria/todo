7852dc3b3e3550acd20ef2f6cd4dd2986570e8ec
msr <mar_r@mail.ru>
2023-05-20 21:40:50 +0300

    first commit

---
+++ src/store/actions/creators/todo.js
@@ -0,0 +1,24 @@
+import { ADD_TODO, TOGGLE_TODO } from "../types/todo";
+
+let nextTodoId = 0;
+
+export const addTodo = (content) => ({
+  type: ADD_TODO,
+  payload: {
+    id: ++nextTodoId,
+    content,
+  },
+});
+
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
+  payload: { id },
+});
+
+// export const deleteTodo = (content) => ({
+//   type: DELETE_TODO,
+//   payload: {
+//     id: --nextTodoId,
+//     content,
+//   },
+// });
---
+++ src/store/reducers/todo.js
@@ -0,0 +1,47 @@
+import { ADD_TODO, TOGGLE_TODO } from "../actions/types/todo";
+
+const initialState = {
+  allId: [],
+  byIds: {},
+};
+
+export default function todoReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO: {
+      const { id, content } = action.payload;
+
+      return {
+        ...state,
+
+        allIds: [...state.allIds, id],
+        byIds: {
+          ...state.byIds,
+          [id]: {
+            content,
+            complete: false,
+          },
+        },
+      };
+    }
+
+    case TOGGLE_TODO: {
+      const { id } = action.payload;
+      const targetTodo = state.byIds[id];
+
+      return {
+        ...state,
+
+        byIds: {
+          ...state.byIds,
+          [id]: {
+            ...targetTodo,
+            completed: !targetTodo.completed,
+          },
+        },
+      };
+    }
+
+    default:
+      break;
+  }
+}
